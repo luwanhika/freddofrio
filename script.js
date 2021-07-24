22f60e48c5dd84888933f3b87ed2119f271d307a
Luwa <luwa@MacBook-Pro-de-Luwa.local>
2021-07-24 13:54:59 -0300

    create form

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const cs = (el)=>document.querySelectorAll(el);
 const mostreVendas = document.querySelector('.vendas_ordens').addEventListener('click', (e)=>{
   e.preventDefault();
 
+  
+
   c('.container').style.opacity = 0;
   c('.container').style.display = 'block';
   setTimeout(()=>{
     c('.container').style.opacity = 1;
   }, 200);
 });
+
